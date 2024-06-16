cdfd3514e3f6458a024ad01e2a3f9d05b108d9f8
Chuts <jesusesparza242@gmail.com>
2024-06-15 22:02:46 -0600

    sdsf

--- fourier.py
+++ fourier.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import math
 import numpy
+import time
+from numba import jit
 
 A = 1
 F = 1000
@@ -8,20 +10,25 @@ T = 1/F
 R = 3300 #ohmns
 C = .000000004 #faradios
 
-Armonicos = 50000
+@jit
+def fourier(Armonicos = 500, lines = 1000):
+    X = numpy.linspace(0, T, lines)
+    Y = []
+    for x in X:
+        f_sum = 0
+        for n in range(1, Armonicos + 1):
+            if n%2 == 1:
+                f = n*F
+                Ganancia = 2*math.pi*f*R*C/math.sqrt(1+2*math.pi*f*R*C)
+                f_sum += Ganancia*4*(math.sin(2*math.pi*f*x))/(n*math.pi)
+            else:
+                pass
+        Y.append(f_sum)
+    return X, Y
 
-X = numpy.linspace(0, T, 1000)
-Y = []
-for x in X:
-    f_sum = 0
-    for n in range(1, Armonicos + 1):
-        if n%2 == 1:
-            f = n*F
-            Ganancia = 2*math.pi*f*R*C/math.sqrt(1+2*math.pi*f*R*C)
-            f_sum += Ganancia*4*(math.sin(2*math.pi*f*x))/(n*math.pi)
-        else:
-            pass
-    Y.append(f_sum)
-
-plt.plot(X, Y)
+t_prev = time.time()
+_X, _Y = fourier(50000, 1000)
+t_diff = time.time() - t_prev
+print(t_diff)
+plt.plot(_X, _Y)
 plt.show()
